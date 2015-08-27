c8c0f637405d852a7320da3d29a75d8de5d02202
Albert Weng <AlbertWeng@airbears2-10-142-18-167.AirBears2.1918.Berkeley.EDU>
2015-08-27 12:59:21 -0700

    fixed

--- audition_site/settings/base.py
+++ audition_site/settings/base.py
@@ -101,7 +101,7 @@ AUTHENTICATION_BACKENDS = (
     "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-INSTALLED_APPShero += (
+INSTALLED_APPS += (
     # The Django sites framework is required
     'django.contrib.sites',
     'allauth',
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
