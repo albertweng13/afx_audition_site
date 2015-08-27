14b9c4cf80f7fc0179773aa8340ded5610c16f0e
Albert Weng <AlbertWeng@Alberts-MacBook-Air.local>
2015-08-27 01:56:33 -0700

    added dj_database_url

--- audition_site/settings/base.py
+++ audition_site/settings/base.py
@@ -104,6 +104,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 AUTHENTICATION_BACKENDS = (
     # Default backend -- used to login by username in Django admin
     "django.contrib.auth.backends.ModelBackend",
@@ -158,24 +161,4 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-)
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 )
